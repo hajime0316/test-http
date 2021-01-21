922513dd1470b1b43f94731873b92c0c20930d85
Hajime <hajime0316@s.okayama-u.ac.jp>
2021-01-21 18:26:55 +0900

    Add function header comment

--- get-hospital-data.js
+++ get-hospital-data.js
@@ -1,3 +1,9 @@
+/**
+ * @brief  Googleスプレッドシートから病院のデータを取得する
+ * @param  url Googleスプレッドシート (GASアプリ) へのURL
+ * @param  hospitalName 病院名
+ * @return 指定した病院の最新のデータが格納された配列
+ */
 async function getHospitalData(url = '', hospitalName = '') {
   const response = await fetch(url + '?hospitalName=' + hospitalName, {
     method: 'GET',
