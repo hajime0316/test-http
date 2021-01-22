d91cc1350ec30d93e04c4a80c1aa6dbacf77b615
Hajime <hajime0316@s.okayama-u.ac.jp>
2021-01-22 12:43:20 +0900

    main.jsにコメントを追加

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// 各GoogleスプレッドシートにアクセスするためのURL
 const urlWardDanger = 'https://script.google.com/macros/s/AKfycbxB-Iu7Vr8jXVA9O067KBdXzIEP83bMMkmYUz1nvpTvvcYpNy5vKXnZ/exec';
 const urlLifeLineSupply = 'https://script.google.com/macros/s/AKfycbwPjAfL5rU5BSa50HIOeXA6WX0qXXkDQN3N13eZD1RJvq8a-eNmray3vA/exec';
 const urlPatientMedicalExamination = 'https://script.google.com/macros/s/AKfycby2vLhNtz7uOZHDbnAs0OPwrCGdyMSO-tMml3MiLor6FSNK-b6Ppdha/exec';
@@ -11,7 +12,11 @@ async function main() {
     $button[i].addEventListener('click', async (e) => {
       let $showResponse = document.getElementById('show-response');
       $showResponse.textContent = '通信中...';
+
+      // Googleスプレッドシートから病院のデータを取ってくる
       const data = await getHospitalData(urlLifeLineSupply, e.target.textContent);
+
+      // 取得したデータを表示
       $showResponse.textContent = '[' + data.join() + '] を受信しました';
     });
   }
