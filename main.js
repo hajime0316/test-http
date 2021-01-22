639d9b6db83a5cf107323c5051b4f0930eb18cd9
Hajime <hajime0316@s.okayama-u.ac.jp>
2021-01-22 12:39:15 +0900

    各GoogleスプレッドシートにアクセスするためのURLを追加

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const urlLifeLineSupply = 'https://script.google.com/macros/s/AKfycbwPjAfL5rU5BSa50HIOeXA6WX0qXXkDQN3N13eZD1RJvq8a-eNmray3vA/exec'
+const urlWardDanger = 'https://script.google.com/macros/s/AKfycbxB-Iu7Vr8jXVA9O067KBdXzIEP83bMMkmYUz1nvpTvvcYpNy5vKXnZ/exec';
+const urlLifeLineSupply = 'https://script.google.com/macros/s/AKfycbwPjAfL5rU5BSa50HIOeXA6WX0qXXkDQN3N13eZD1RJvq8a-eNmray3vA/exec';
+const urlPatientMedicalExamination = 'https://script.google.com/macros/s/AKfycby2vLhNtz7uOZHDbnAs0OPwrCGdyMSO-tMml3MiLor6FSNK-b6Ppdha/exec';
+const urlStaffNumber = 'https://script.google.com/macros/s/AKfycbzAHB2JNiJjGrePNxE0mkXJV3staxbllC75drSCZdwZHu-PPbnU3uiK/exec';
+const urlOther = 'https://script.google.com/macros/s/AKfycbzd_yM_eB0T7_-fQFEla2EuoefCD1P7dXNmjzEeqZp1xFQ2L-qEXWcs/exec';
 
 async function main() {
   const $button = document.getElementsByTagName('button');
